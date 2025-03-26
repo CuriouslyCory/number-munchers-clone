5e1626ca05c81928014de3d210f8a75e120b4fa3
CuriouslyCory <cory@hau.me>
2025-03-26 14:04:05 -0700

    Remove redundant globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
-import type React from "react"
-import "~/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "~/components/theme-provider"
+import type React from "react";
+import "~/styles/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "~/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Number Munchers",
   description: "A web remake of the classic DOS game",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
